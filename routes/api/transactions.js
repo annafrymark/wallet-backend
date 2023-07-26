98c4bf17e4590f279963fc1c4c28da9a4b170f38
PaulinaStachPluskowska <109916798+PaulinaStachPluskowska@users.noreply.github.com>
2023-07-26 22:30:08 +0200

    fix

--- routes/api/transactions.js
+++ routes/api/transactions.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const controllTransaction = require("../../controller");
+const controllTransaction = require("../../controller/index");
 const auth = require("../../middleware-psp/auth/auth");
 
 router.post("/", auth, controllTransaction.addTransaction);
